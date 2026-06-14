10eca14d1a3c3a72cebd1639653b70fa4e556576
fullone123 <goal83544@gmail.com>
2026-06-14 04:31:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ WEBAPP_URL      = os.getenv("WEBAPP_URL", "http://localhost:8000").rstrip("/")
 PROXYCHECK_KEY  = os.getenv("PROXYCHECK_API_KEY", "")
 
 # 📸 የቴሌብር ፕሩፍ ፎቶ (መጀመሪያ ሊንክ ነው፤ ቦቱ የሚሰጥህን File ID እዚህ ላይ መተካት ትችላለህ)
-TELEBIRR_PROOF_IMAGE = os.getenv("TELEBIRR_PROOF_IMAGE", "https://i.imgur.com/8bX9K4m.jpg")
+TELEBIRR_PROOF_IMAGE = os.getenv("TELEBIRR_PROOF_IMAGE", "AgACAgQAAxkBAAOYai38ooud5iofBd3aDGuCiX273t8AAj4PaxsYl3BR78MpfA_cDpkBAAMCAAN4AAM8BA")
 
 if WEBAPP_URL.startswith("tg56") or not WEBAPP_URL.startswith(("http://", "https://")):
     WEBAPP_URL = f"https://{WEBAPP_URL}"
